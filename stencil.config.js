8ddcdbf2cdf9bc72b20cfb19c810ee1c4455401e
tamerbakdev <tamer.development@gmail.com>
2018-02-13 18:41:17 +0200

    Remove bundles config

--- stencil.config.js
+++ stencil.config.js
@@ -1,9 +1,4 @@
 exports.config = {
-  bundles: [
-    { components: ['mca-app', 'mca-app-header', 'mca-home', 'mca-data', 'mca-loader', 'mca-editor'] },
-    { components: ['mca-form-container', 'mca-form-text-control'] },
-    { components: ['mca-data-table', 'mca-data-table-view', 'mca-data-table-header', 'mca-data-table-filter', 'mca-data-table-pager'] }
-  ],
   collections: [
     { name: '@stencil/router' }
   ]
