193b73d55ff4abe5e12fcae13ad8db18e6472cae
tamerbakdev <tamer.development@gmail.com>
2018-02-14 12:57:56 +0200

    Enhance code formatting. Implement isFunction, isTruthy, hasProperty, hasMethod funcitons

--- src/components/mca-data-utils.ts
+++ src/components/mca-data-utils.ts
@@ -1,16 +1,54 @@
-export const isNumber = (value: any) => typeof value === 'number' && !Number.isNaN(value);
+export const isNumber = (value: any) => {
+  return typeof value === 'number' && !Number.isNaN(value);
+}
 
-export const isEmptyArray = (arr: any) => Array.isArray(arr) && arr.length < 1;
+export const isEmptyArray = (arr: any) => {
+  return Array.isArray(arr) && arr.length < 1;
+}
 
-export const isValidString = (str: any) => typeof str === 'string' && str.length > 0;
+export const isValidString = (str: any) => {
+  return typeof str === 'string' && str.length > 0;
+}
 
-export const isObject = (obj: any) => typeof obj === 'object' && obj instanceof Object && Object.keys(obj).length > 0;
+export const isObject = (obj: any) => {
+  return typeof obj === 'object'
+    && obj instanceof Object
+    && Object.keys(obj).length > 0;
+}
 
-export const snakeToTitleCase = (str: string) => str
-  .replace('_', ' ')
-  .split(' ')
-  .map(word => word.charAt(0).toUpperCase() + word.slice(1, word.length))
-  .join(' ')
+export const isFunction = (func: any) => {
+  return typeof func === 'function';
+}
+
+export const isTruthy = (value: any) => {
+  return value !== false
+    && value != null
+    && value !== 0
+    && value !== ''
+    && Number.isNaN(value);
+}
+
+export const hasProperty = (obj: any, prop: string) => {
+  return isObject(obj)
+    && isValidString(prop)
+    && prop in obj
+    && isTruthy(obj[prop]);
+}
+
+export const hasMethod = (obj: any, methodName: any) => {
+  return isObject(obj)
+    && isValidString(methodName)
+    && methodName in obj
+    && isFunction(obj[methodName]);
+}
+
+export const snakeToTitleCase = (str: string) => {
+  return str
+    .replace('_', ' ')
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1, word.length))
+    .join(' ');
+}
 
 /** level 0: default, 1: primary, 2: success, 3: warn, 4: error */
 export const log = (level = 0, ...messageParts) => {
